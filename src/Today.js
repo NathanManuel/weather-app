5b630572ee5ac119d3484f11aefaed6524e4c99f
Nathan <nathanvanvelden@gmail.com>
2023-04-10 14:17:14 +0300

    added tailwind and created slider for todays forcast

---
+++ src/Today.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import "./style.css";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+
+const Today = ({ todayData, fahrenheit }) => {
+  const slideLeft = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+  return (
+    <div>
+      {todayData ? (
+        <div>
+          <div className="day">Today Forcast</div>
+          <div className="relative flex items-center">
+            <MdChevronLeft
+              className="opacity-50 cursor-pointer hover:opacity-100"
+              onClick={slideLeft}
+              color="white"
+              size={30}
+            />
+            <div
+              id="slider"
+              className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide"
+            >
+              {todayData.hour.map((weather, i) => {
+                const dateNow = new Date();
+                const now = dateNow.getHours();
+                const date = new Date(weather.time);
+                const hour = String(date.getHours()).padStart(2, "0");
+                const minute = String(date.getMinutes()).padStart(2, "0");
+                return (
+                  <div key={i} className="inline-block description">
+                    {!fahrenheit ? (
+                      <p> {Math.ceil(weather.temp_c)} &deg;C</p>
+                    ) : (
+                      <p> {Math.ceil(weather.temp_f)} &deg;F</p>
+                    )}
+                    <img
+                      src={weather.condition.icon}
+                      alt="Weather data by WeatherAPI.com"
+                      border="0"
+                    />
+                    {String(now) === hour ? (
+                      <p className="now">
+                        {hour}: {minute}
+                      </p>
+                    ) : (
+                      <p>
+                        {hour}: {minute}
+                      </p>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+            <MdChevronRight
+              className="opacity-50 cursor-pointer hover:opacity-100"
+              onClick={slideRight}
+              color="white"
+              size={30}
+            />
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div className="forcastText">Forcast today</div>
+          <div className="date">Loading...</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Today;
