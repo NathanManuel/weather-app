2c982248e8c7dc9c8134a1a0ec93ad2bcb86cd43
Nathan <nathanvanvelden@gmail.com>
2023-04-11 15:32:44 +0300

    change weather item display

--- src/Weather.js
+++ src/Weather.js
@@ -1,73 +1,77 @@
 import React, { useEffect } from "react";
 import moment from "moment";
 import "./style.css";
+import Today from "./Today";
+import WeekForcast from "./WeekForcast";
 
-const refresh = () => {
-  window.location.reload();
-};
-
-const CardExampleCard = ({ weatherData }) => {
+const Weather = ({ weatherData, fahrenheit }) => {
   return (
     <div className="main">
       {weatherData && (
         <div>
           <div className="top">
             <p className="header">
-              {weatherData.name} : {weatherData.sys.country}
+              {weatherData.location.name} : {weatherData.location.country}
             </p>
+            <div className="header">
+              {!fahrenheit ? (
+                <div>{Math.ceil(weatherData.current.temp_c)} &deg;C</div>
+              ) : (
+                <div> {Math.ceil(weatherData.current.temp_f)} &deg;F</div>
+              )}
+            </div>
           </div>
           <div className="flex date">
-            <p className="day">
-              {moment().format("dddd")}, <span>{moment().format("LL")}</span>
-            </p>
-            <div className="f">
-              <p className="description">{weatherData.weather[0].main}:</p>
-              <p className="description">
-                {weatherData.weather[0].description}
-              </p>
-            </div>
+            <p className="temp">{weatherData.current.condition.text}</p>
             <img
-              src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`}
+              src={weatherData.current.condition.icon}
+              alt="Weather data by WeatherAPI.com"
+              border="0"
             />
           </div>
-
-          <div className="flex">
-            <p className="temp">Temprature: {weatherData.main.temp} &deg;C</p>
-
-            <p className="temp">Humidity: {weatherData.main.humidity} %</p>
-          </div>
-          <div className="flex">
-            <p className="temp">
-              Feels like: {weatherData.main.feels_like} &deg;C
-            </p>
-          </div>
-          <div className="flex">
-            <p className="temp">
-              Min temprature: {weatherData.main.temp_min} &deg;C
-            </p>
-            <p className="temp">
-              Max temprature: {weatherData.main.temp_max} &deg;C
-            </p>
-          </div>
-
-          <div className="flex">
-            <p className="sunrise-sunset">
-              Sunrise:{" "}
-              {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString(
-                "en-IN"
+          <div className="">
+            <div className="temp flex">
+              Temprature:
+              {!fahrenheit ? (
+                <div> {Math.ceil(weatherData.current.temp_c)} &deg;C</div>
+              ) : (
+                <div> {Math.ceil(weatherData.current.temp_f)} &deg;F</div>
               )}
-            </p>
-            <p className="sunrise-sunset">
-              Sunset:{" "}
-              {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString(
-                "en-IN"
+            </div>
+            <div className="temp flex">
+              Feels like:
+              {!fahrenheit ? (
+                <div> {Math.ceil(weatherData.current.feelslike_c)} &deg;C</div>
+              ) : (
+                <div> {Math.ceil(weatherData.current.feelslike_f)} &deg;F</div>
               )}
-            </p>
+            </div>
           </div>
+          <div className="flex">
+            <div className="temp flex">
+              Wind:
+              {!fahrenheit ? (
+                <div> {Math.ceil(weatherData.current.wind_kph)} km/h</div>
+              ) : (
+                <div> {Math.ceil(weatherData.current.wind_mph)} mp/h</div>
+              )}
+            </div>
+            <p className="temp">Humidity: {weatherData.current.humidity} %</p>
+          </div>
+
+          <Today
+            todayData={weatherData.forecast.forecastday[0]}
+            fahrenheit={fahrenheit}
+          />
+
+          <WeekForcast
+            week={weatherData.forecast.forecastday}
+            fahrenheit={fahrenheit}
+          />
         </div>
       )}
     </div>
   );
 };
 
-export default CardExampleCard;
+export default Weather;
