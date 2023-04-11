ac7b859d408a0b6b239e273dac76072093e64c43
Nathan <nathanvanvelden@gmail.com>
2023-04-11 17:25:26 +0300

    changed location finder, optimized code

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Weather from "./Weather";
 import "./homeStyle.css";
 
 export default function App() {
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
   const [wData, setWData] = useState();
   const [input, setInput] = useState("");
   const [isPending, setIsPending] = useState(true);
@@ -13,83 +11,82 @@ export default function App() {
   const [local, setLocal] = useState();
   const [fahrenheit, setFahrenheit] = useState(false);
 
-  const fetchData = async () => {
-    try {
-      setIsPending(true);
-      const response = await fetch(
-        `https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API}&q=${input}&days=7&aqi=no&alerts=no`
-      );
-      const jsonD = await response.json();
-      setWData(jsonD);
-      setIsPending(false);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const fetchData = useCallback(
+    async (city) => {
+      try {
+        setIsPending(true);
+        const response = await fetch(
+          `https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API}&q=${city}&days=7&aqi=no&alerts=no`
+        );
+        const jsonD = await response.json();
+        setWData(jsonD);
+        setIsPending(false);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    [setWData, setIsPending]
+  );
 
-  const localData = useCallback(async () => {
+  const getCityByIP = useCallback(async () => {
     try {
-      setIsPending(true);
-      const response = await fetch(
-        `https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API}&q=${local}&days=7&aqi=no&alerts=no`
-      );
-      const jsonD = await response.json();
-      setWData(jsonD);
-      setIsPending(false);
-    } catch (error) {
-      console.error(error);
-    }
-  }, [setWData, setIsPending, local]);
-
-  const getLocation = useCallback(async () => {
-    try {
-      const url = `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${long}&key=${process.env.REACT_APP_LOCATION_API}`;
-      const response = await fetch(url);
+      const response = await fetch("https://ipapi.co/json/");
       const data = await response.json();
-      const city = data.results[0].components.city;
-      setLocal(city);
+      const city = data.city;
+      return city;
     } catch (error) {
       console.error(error);
     }
-  }, [lat, long, setLocal]);
+  }, []);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
+    getCityByIP().then((city) => {
+      setLocal(city);
     });
-  }, [setLat, setLong]);
-
+  }, [getCityByIP, setLocal]);
   useEffect(() => {
-    getLocation();
-  }, [getLocation]);
+    if (local) {
+      fetchData(local);
+    }
+  }, [local, fetchData]);
 
   return (
     <div className="App">
       <div className="topH">
         <h1 className="local">Weather app</h1>
         <div>
-          <input
-            type="text"
-            value={input}
-            placeholder="City name..."
-            onChange={(e) => setInput(e.target.value)}
-          />
+          <div>
+            <input
+              type="text"
+              className="cityInput"
+              value={input}
+              placeholder="City name..."
+              onChange={(e) => setInput(e.target.value)}
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  fetchData(input);
+                }
+              }}
+            />
+            <button
+              onClick={() => {
+                fetchData(input);
+              }}
+            >
+              Search
+            </button>
+          </div>
           <button
             onClick={() => {
-              fetchData();
-            }}
-          >
-            Search
-          </button>
-          <button
-            onClick={() => {
-              localData();
+              fetchData(local);
             }}
           >
             Local Weather
           </button>
-          <button onClick={() => setFahrenheit(!fahrenheit)}>
+          <button
+            className="weatherType"
+            onClick={() => setFahrenheit(!fahrenheit)}
+          >
             {!fahrenheit ? <p>&deg;C</p> : <p>&deg;F</p>}
           </button>
         </div>
